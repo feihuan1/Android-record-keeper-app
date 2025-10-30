0f3e8bb43b5c01df58ca5d8fdba6f2073e482aa0
Feihuan Peng <feihuan.peng@spreetail.com>
2025-10-30 00:40:04 -0500

    asd

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recordkeeper
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.textViewExample.text = "text changed"
     }
 }
