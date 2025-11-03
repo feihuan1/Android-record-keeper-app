87d08ee75c77b226a638ee631b8c02546ce06a0a
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-02 19:55:30 -0600

    asd

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.buttonCycling.setOnClickListener { onCyclingClicked() }
         binding.buttonRunning.setOnClickListener { onRunningClicked() }
     }
