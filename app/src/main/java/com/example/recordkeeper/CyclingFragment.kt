8868fa862094490ab3eb474d087ad37bb191b078
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-12 23:46:49 -0600

    asd

--- app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
+++ app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
@@ -1,20 +1,42 @@
 package com.example.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recordkeeper.databinding.FragmentCyclingBinding
 
 
 class CyclingFragment: Fragment() {
 
+    private  lateinit var binding: FragmentCyclingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_cycling, container,false)
+        binding = FragmentCyclingBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+        binding.containerLongestRide.setOnClickListener { launchEditCyclingRecordScreen("Longest Ride") }
+        binding.containerLongestRide.setOnClickListener { launchEditCyclingRecordScreen("Biggest Climb") }
+        binding.containerLongestRide.setOnClickListener { launchEditCyclingRecordScreen("Best Average Speed") }
+    }
+    private fun launchEditCyclingRecordScreen(record: String) {
+        val intent = Intent(context, EditRunningRecordAtivity::class.java)
+        intent.putExtra("Record", record)
+        startActivity(intent)
+    }
+
 }
+
---
+++ app/src/main/java/com/example/recordkeeper/EditCyclingRecordActivity.kt
@@ -0,0 +1,23 @@
+package com.example.recordkeeper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.recordkeeper.databinding.ActivityEditCyclingRecordBinding
+
+class EditCyclingRecordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditCyclingRecordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditCyclingRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val record = intent.getStringExtra("Record")
+        title = "$record Record"
+
+    }
+}
