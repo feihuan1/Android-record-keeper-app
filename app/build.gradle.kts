530134df91f5e7481697869eb290ba61d794549e
Feihuan Peng <feihuan.peng@spreetail.com>
2025-12-01 23:48:31 -0600

    asd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment.ktx)
+    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/recordkeeper/running/EditRunningRecordAtivity.kt
+++ app/src/main/java/com/example/recordkeeper/running/EditRunningRecordAtivity.kt
@@ -1,9 +1,12 @@
 package com.example.recordkeeper.running
 
 import android.R
+import android.content.Context
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.example.recordkeeper.databinding.ActivityEditRunningRecordAtivityBinding
 
 class EditRunningRecordAtivity : AppCompatActivity() {
@@ -12,7 +15,7 @@ class EditRunningRecordAtivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityEditRunningRecordAtivityBinding.inflate(layoutInflater)
+		binding = ActivityEditRunningRecordAtivityBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		setSupportActionBar(binding.toolbar)
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -20,6 +23,24 @@ class EditRunningRecordAtivity : AppCompatActivity() {
 		supportActionBar?.setDisplayShowTitleEnabled(true)
         val distance = intent.getStringExtra("Distance")
 		supportActionBar?.title = "$distance Record"
+
+		val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+		applicationPreferences.edit {
+			putString("some data", "Applications preference value")
+		}
+
+		val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+
+		activityPreferences.edit {
+			putInt("ActivityData", 1)
+		}
+
+		val fileNamePreferences = getSharedPreferences("some shared preference file name", Context.MODE_PRIVATE)
+
+		fileNamePreferences.edit {
+			putBoolean("Some Preference file name data", false)
+		}
     }
 
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
