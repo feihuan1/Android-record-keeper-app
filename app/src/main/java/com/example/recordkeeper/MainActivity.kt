10732cee175843334b7e3616544df4fb5a62f82f
Feihuan Peng <feihuan.peng@spreetail.com>
2025-12-09 22:29:31 -0600

    asd

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.recordkeeper
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.core.content.ContextCompat
+import androidx.core.content.edit
 import androidx.core.graphics.drawable.DrawableCompat
 import com.example.recordkeeper.cycling.CyclingFragment
 import com.example.recordkeeper.databinding.ActivityMainBinding
@@ -45,20 +46,21 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         else -> false
     }
 
-    override fun onOptionsItemSelected(item: MenuItem) =
-        when (item.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuClickedHandled = when (item.itemId) {
             R.id.reset_running -> {
-                Toast.makeText(this, "Clicked reset running", Toast.LENGTH_LONG).show()
+                getSharedPreferences("running", Context.MODE_PRIVATE).edit { clear() }
                 true
             }
 
             R.id.reset_cycling -> {
-                Toast.makeText(this, "Clicked reset cycling", Toast.LENGTH_LONG).show()
+                getSharedPreferences("cycling", Context.MODE_PRIVATE).edit { clear() }
                 true
             }
 
             R.id.reset_all -> {
-                Toast.makeText(this, "Clicked reset all", Toast.LENGTH_LONG).show()
+                getSharedPreferences("running", Context.MODE_PRIVATE).edit { clear() }
+                getSharedPreferences("running", Context.MODE_PRIVATE).edit { clear() }
                 true
             }
 
@@ -66,6 +68,18 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
                 super.onOptionsItemSelected(item)
             }
         }
+
+        when (binding.bottomNav.selectedItemId) {
+            R.id.nav_running ->  onRunningClicked()
+            R.id.nav_cycling ->  onCyclingClicked()
+            else -> {
+                onRunningClicked()
+                onCyclingClicked()
+            }
+        }
+
+        return menuClickedHandled
+    }
 
 
     private fun onRunningClicked(): Boolean {
